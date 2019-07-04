f314c9ce03bc1fb57f3551e3e76fe768b95280cb
David Quintanilla <david.quinta415@gmail.com>
2019-07-04 06:36:45 -0600

    Update Dashboard

---
+++ firebase-init.js
@@ -0,0 +1,14 @@
+
+        // Your web app's Firebase configuration
+        var firebaseConfig = {
+          apiKey: "AIzaSyDzs9iemsa4srBM5TnsJ5iOp3FHZh_0B4w",
+          authDomain: "asurafaceid.firebaseapp.com",
+          databaseURL: "https://asurafaceid.firebaseio.com",
+          projectId: "asurafaceid",
+          storageBucket: "asurafaceid.appspot.com",
+          messagingSenderId: "965905618234",
+          appId: "1:965905618234:web:8a39490375360025"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        const db = firebase.firestore();
